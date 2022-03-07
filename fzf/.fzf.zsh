6f4d440596114bf2e0aca5789e533717c0566f2d
Hristo Goumnerov <goumnerov@gmail.com>
2022-03-07 09:50:18 +0100

    generalized user for fzf

--- fzf/.fzf.zsh
+++ fzf/.fzf.zsh
@@ -1,15 +1,15 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/user/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/user/.fzf/bin"
+if [[ ! "$PATH" == */home/gou/.fzf/bin* ]]; then
+    export PATH="${PATH:+${PATH}:}/home/$(whoami)/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/user/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/home/$(whoami)/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/user/.fzf/shell/key-bindings.zsh"
+source "/home/$(whoami)/.fzf/shell/key-bindings.zsh"
 
 export FZF_CTRL_T_OPTS='--preview "bat --style=numbers --color=always --line-range :500 {}"'
