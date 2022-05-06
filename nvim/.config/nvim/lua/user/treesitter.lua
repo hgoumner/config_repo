ceb4005c6b3010bc7ddb6c5840d450beb5d86cfa
Hristo Goumnerov <goumnerov@gmail.com>
2022-05-06 22:04:13 +0200

    added treesitter settings, textobjects

--- nvim/.config/nvim/lua/user/treesitter.lua
+++ nvim/.config/nvim/lua/user/treesitter.lua
@@ -6,23 +6,56 @@ end
 configs.setup {
     ensure_installed = "all",
     sync_install = false,
-    ignore_install = { "" }, -- List of parsers to ignore installing
+    ignore_install = { "" }, -- list of parsers to ignore installing
     highlight = {
         enable = true, -- false will disable the whole extension
         disable = { "" }, -- list of language that will be disabled
         additional_vim_regex_highlighting = true,
-
-  },
-  indent = { enable = true, disable = { "yaml" } },
-  rainbow = {
-    enable = true,
-    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- colors = {}, -- table of hex strings
-    -- termcolors = {} -- table of colour name strings
-  },
-  playground = {
-      enable = true
-  }
+    },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = 'gnn',
+            node_incremental = 'grn',
+            scope_incremental = 'grc',
+            node_decremental = 'grm',
+        },
+    },
+    indent = {
+        enable = true,
+        disable = { "yaml" }
+    },
+    textobjects = {
+	    select = {
+		    enable = true,
+		    lookahead = true, -- automatically jump forward to textobj, similar to targets.vim
+		    keymaps = {
+			    -- you can use the capture groups defined in textobjects.scm
+			    ['af'] = '@function.outer',
+			    ['if'] = '@function.inner',
+			    ['ac'] = '@class.outer',
+			    ['ic'] = '@class.inner',
+		    },
+	    },
+	    move = {
+		    enable = true,
+		    set_jumps = true, -- whether to set jumps in the jumplist
+		    goto_next_start = {
+			    [']m'] = '@function.outer',
+			    [']]'] = '@class.outer',
+		    },
+		    goto_next_end = {
+			    [']m'] = '@function.outer',
+			    [']['] = '@class.outer',
+		    },
+		    goto_previous_start = {
+			    ['[m'] = '@function.outer',
+			    ['[['] = '@class.outer',
+		    },
+		    goto_previous_end = {
+			    ['[m'] = '@function.outer',
+			    ['[]'] = '@class.outer',
+		    },
+	    },
+    },
 }
