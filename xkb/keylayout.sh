6d73c9c66d35a5f613479d93307a4fe74f30ab16
Hristo Goumnerov <goumnerov@gmail.com>
2022-08-05 14:00:31 +0200

    added german

--- xkb/keylayout.sh
+++ xkb/keylayout.sh
@@ -6,4 +6,4 @@ setxkbmap -option caps:escape
 # German programmer's dvorak and bulgarian dvorak
 sudo cp ~/config_repo/xkb/de_dvp /usr/share/X11/xkb/symbols
 sudo cp ~/config_repo/xkb/bg_dvorak /usr/share/X11/xkb/symbols
-setxkbmap -verbose -layout "de_dvp(dvp),bg_dvorak(dvorak)" -option "grp:shifts_toggle"
+setxkbmap -verbose -layout "de_dvp(dvp),bg_dvorak(dvorak), de" -option "grp:shifts_toggle"
