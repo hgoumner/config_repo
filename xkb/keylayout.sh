e3718ee80d4d1ca8fdc2e3371fd079fc083c3b0e
Hristo Goumnerov <goumnerov@gmail.com>
2022-09-01 10:08:19 +0200

    added screen clearing in keylayout

--- xkb/keylayout.sh
+++ xkb/keylayout.sh
@@ -7,3 +7,6 @@ setxkbmap -option caps:escape
 sudo cp ~/config_repo/xkb/de_dvp /usr/share/X11/xkb/symbols
 sudo cp ~/config_repo/xkb/bg_dvorak /usr/share/X11/xkb/symbols
 setxkbmap -verbose -layout "de_dvp(dvp),bg_dvorak(dvorak), de" -option "grp:shifts_toggle"
+
+sleep 3
+printf "\033c"
