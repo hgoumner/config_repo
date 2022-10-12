1dba9b43410e234aa292e320c5e3ec34b1760f7f
Hristo Goumnerov <goumnerov@gmail.com>
2022-10-12 13:05:32 +0200

    add mapping to show all characters

--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -44,6 +44,8 @@ for k, v in pairs(options) do
     vim.opt[k] = v
 end
 
+vim.cmd [[set listchars=eol:¬,tab:>·,trail:~,extends:>,precedes:<,space:␣]]
+vim.cmd [[set colorcolumn=79]]
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work:
