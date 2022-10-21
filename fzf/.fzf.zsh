725dbaf278d976e4424ea4eaf580d531ae59f0ab
Hristo Goumnerov <goumnerov@gmail.com>
2022-10-21 20:56:01 +0200

    better rga fzf search

--- fzf/.fzf.zsh
+++ fzf/.fzf.zsh
@@ -20,41 +20,62 @@ export FZF_CTRL_T_OPTS='--preview "bat --style=numbers --color=always --line-ran
 export FZF_ALT_C_COMMAND='fd --type d --hidden --follow --exclude BraveSoftware --exclude .git'
 export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
 
-# rg-fzf() {
-#     INITIAL_QUERY=""
-#     RG_PREFIX="rg --files-with-matches --hidden --column --line-number --no-heading --color=always --smart-case "
-#     FZF_DEFAULT_COMMAND="$RG_PREFIX '$INITIAL_QUERY'" \
-#         fzf --preview "bat --style=numbers --color=always --line-range=:500 {}" --bind "change:reload:$RG_PREFIX {q} || true" \
-#         --ansi --disabled --query "$INITIAL_QUERY"
+rga-fzf-all() {
+    local match=$(
+      rga --hidden --column --no-heading --color=always --smart-case "${*:-}" |
+        fzf --ansi --delimiter : --nth 3.. \
+            --preview "[[ ! -z {} ]] && rga --pretty --line-number --context 5 {q} {1}"
+      )
+    local file=$(echo "$match" | cut -d':' -f1)
+    if [[ -n $file ]]; then
+        $EDITOR "$file" +$(echo "$match" | cut -d':' -f2)
+    fi
+}
+
+# rga-fzf-all() {
+# 	RG_PREFIX="rga --hidden --files-with-matches --column --line-number --no-heading --color=always"
+# 	local file
+# 	file="$(
+# 		FZF_DEFAULT_COMMAND="$RG_PREFIX '$1'" \
+# 			fzf --ansi --sort --preview="[[ ! -z {} ]] && rga --pretty --context 5 {q} {}" \
+# 				--phony -q "$1" \
+# 				--bind "change:reload:$RG_PREFIX {q}"
+# 	)" &&
+# 	echo "opening $file" &&
+# 	xdg-open "$file"
+# }
+#
+# rg-fzf-nvim(){
+#     local match=$(
+#       rg --column --line-number --no-heading --color=always --smart-case "${*:-}" |
+#         fzf --ansi --delimiter : \
+#             --preview "nvim +{2} {1}"
+#       )
+#     local file=$(echo "$match" | cut -d':' -f1)
+#     if [[ -n $file ]]; then
+#         $EDITOR "$file" +$(echo "$match" | cut -d':' -f2)
+#     fi
+# }
+#
+# rg-fzf-bat(){
+#     local match=$(
+#       rg --column --line-number --no-heading --color=always --smart-case "${*:-}" |
+#         fzf --ansi --delimiter : \
+#             --preview "bat --style=numbers --color=always --highlight-line {2} {1}"
+#       )
+#     local file=$(echo "$match" | cut -d':' -f1)
+#     if [[ -n $file ]]; then
+#         $EDITOR "$file" +$(echo "$match" | cut -d':' -f2)
+#     fi
 # }
 #
 
-rg-fzf-nvim(){
-    local match=$(
-      rg --column --line-number --no-heading --color=always --smart-case "${*:-}" |
-        fzf --ansi --delimiter : \
-            --preview "nvim +{2} {1}"
-      )
-    local file=$(echo "$match" | cut -d':' -f1)
-    if [[ -n $file ]]; then
-        $EDITOR "$file" +$(echo "$match" | cut -d':' -f2)
-    fi
-}
+zle     -N  rga-fzf-all
+bindkey '^H' rga-fzf-all
 
-rg-fzf-bat(){
-    local match=$(
-      rg --column --line-number --no-heading --color=always --smart-case "${*:-}" |
-        fzf --ansi --delimiter : \
-            --preview "bat --style=numbers --color=always --highlight-line {2} {1}"
-      )
-    local file=$(echo "$match" | cut -d':' -f1)
-    if [[ -n $file ]]; then
-        $EDITOR "$file" +$(echo "$match" | cut -d':' -f2)
-    fi
-}
-
-zle     -N  rg-fzf-bat
-bindkey '^P' rg-fzf-bat
-
-zle     -N  rg-fzf-nvim
-bindkey '^H' rg-fzf-nvim
+# zle     -N  rg-fzf-bat
+# bindkey '^P' rg-fzf-bat
+#
+#
+# zle     -N  rg-fzf-nvim
+# bindkey '^H' rg-fzf-nvim
