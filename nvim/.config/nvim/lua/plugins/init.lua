441a812666fa0ef66ada6a7d9832f3897ef95455
Hristo Goumnerov <goumnerov@gmail.com>
2023-03-06 13:30:14 +0100

    nvim header change

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -263,6 +263,7 @@ return {
         'mg979/vim-visual-multi',
         lazy = false,
     },
+
 }
 
 -- -------------------
---
+++ nvim/.config/nvim/lua/plugins/vimtex.lua
@@ -0,0 +1,14 @@
+return {
+    'lervag/vimtex',
+    ft = { 'tex' },
+    config = function()
+
+        -- PDF viewer
+        -- vim.g.vimtex_view_general_viewer = 'org.kde.okular'
+        -- vim.g.vimtex_view_general_options = '--unique file:@pdf#src:@line@tex'
+
+        -- map local leader
+        vim.g.maplocalleader = ','
+
+    end,
+}
