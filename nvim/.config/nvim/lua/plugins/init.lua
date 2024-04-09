c1f4a0116713479c4cac4d3e62ecad78ed59d747
Hristo Goumnerov <hristo.g@pharvision.ai>
2024-04-09 07:06:15 -0500

    Nvim - fix notify warning

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -168,5 +168,40 @@ return {
     {
         'rcarriga/nvim-notify',
         lazy = false,
-    }
+        config = function()
+
+            vim.opt.termguicolors = true
+
+            require("notify").setup {
+                background_colour = "#000000",
+            }
+        end,
+    },
+
+    {
+        "lukoshkin/highlight-whitespace",
+        lazy = false,
+        -- config=true,
+        opts = {
+            tws = "\\s\\+$",
+            clear_on_winleave = false,
+            palette = {
+                markdown = {
+                    tws = 'RosyBrown',
+                    ['\\S\\@<=\\s\\(\\.\\|,\\)\\@='] = 'CadetBlue3',
+                    ['\\S\\@<= \\{2,\\}\\S\\@='] = 'SkyBlue1',
+                    ['\\t\\+'] = 'plum4',
+                },
+                other = {
+                    -- tws = 'PaleVioletRed',
+                    tws = 'Red',
+                    ['\\S\\@<=\\s,\\@='] = 'coral1',
+                    ['\\S\\@<=\\(#\\|--\\)\\@<! \\{2,3\\}\\S\\@=\\(#\\|--\\)\\@!'] = 'LightGoldenrod3',
+                    ['\\(#\\|--\\)\\@<= \\{2,\\}\\S\\@='] = '#3B3B3B',
+                    ['\\S\\@<= \\{3,\\}\\(#\\|--\\)\\@='] = '#3B3B3B',
+                    ['\\t\\+'] = 'plum4',
+                }
+            }
+  }
+    },
 }
