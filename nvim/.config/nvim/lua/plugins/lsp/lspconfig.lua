6be2a5d34f04e4d68d848a994eae0d0304160970
Hristo Goumnerov <hristo.g@pharvision.ai>
2024-06-05 07:18:07 -0500

    Nvim - change to pylsp

--- nvim/.config/nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -67,13 +67,47 @@ return {
           vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
         end
 
-        lspconfig['ruff_lsp'].setup({
-            init_options = {
-                settings = {
-                    args = {},
+        lspconfig["pylsp"].setup {
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = {
+                pylsp = {
+                    plugins = {
+                        pycodestyle = {
+                            ignore = {
+                                'E402',
+                                'W391'
+                            },
+                            maxLineLength = 120
+                        },
+                        ruff = {
+                            enabled = true,  -- Enable the plugin
+                            formatEnabled = true,  -- Enable formatting using ruffs formatter
+                            executable = "<path-to-ruff-bin>",  -- Custom path to ruff
+                            config = "<path_to_custom_ruff_toml>",  -- Custom config for ruff to use
+                            extendSelect = { "I" },  -- Rules that are additionally used by ruff
+                            extendIgnore = { "C90" },  -- Rules that are additionally ignored by ruff
+                            format = { "I" },  -- Rules that are marked as fixable by ruff that should be fixed when running textDocument/formatting
+                            severities = { ["D212"] = "I" },  -- Optional table of rules where a custom severity is desired
+                            unsafeFixes = false,  -- Whether or not to offer unsafe fixes as code actions. Ignored with the "Fix All" action
+
+                            -- Rules that are ignored when a pyproject.toml or ruff.toml is present:
+                            lineLength = 120,  -- Line length to pass to ruff checking and formatting
+                            exclude = { "__about__.py" },  -- Files to be excluded by ruff checking
+                            select = { "F" },  -- Rules to be enabled by ruff
+                            ignore = {
+                                'E402',
+                                'W391',
+                                "D210"
+                            },  -- Rules to be ignored by ruff
+                            perFileIgnores = { ["__init__.py"] = "CPY001" },  -- Rules that should be ignored for specific files
+                            preview = true,  -- Whether to enable the preview style linting and formatting.
+                            targetVersion = "py310",  -- The minimum python version to target (applies for both linting and formatting).
+                        },
+                    }
                 }
             }
-        })
+        }
 
         lspconfig["lua_ls"].setup({
             capabilities = capabilities,
