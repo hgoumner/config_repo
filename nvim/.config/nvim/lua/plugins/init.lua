a75c7c0cf0801b4cd09b3ea19332933edd931c4b
Hristo Goumnerov <hristo.g@pharvision.ai>
2024-08-08 22:35:53 +0300

    Nvim - use other gruvbox

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -3,20 +3,51 @@ return {
     -- Colorscheme settings --
     --------------------------
 
+    -- {
+    --     'sainnhe/gruvbox-material',
+    --     lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    --     priority = 1000, -- make sure to load this before all the other start plugins
+    --     config = function()
+    --         -- load the colorscheme here
+    --         vim.cmd('set background=dark')
+    --         vim.cmd('let g:gruvbox_material_background = "soft"')
+    --         vim.cmd('let g:gruvbox_material_better_performance = 1')
+    --         vim.cmd('let g:gruvbox_material_transparent_background = 2')
+    --         vim.cmd('let g:gruvbox_material_current_word = "bold"')
+    --         vim.cmd('colorscheme gruvbox-material')
+    --     end,
+    -- },
     {
-        'sainnhe/gruvbox-material',
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
+        "ellisonleao/gruvbox.nvim",
+        lazy = false,
+        priority = 1000,
         config = function()
-            -- load the colorscheme here
-            vim.cmd('set background=dark')
-            vim.cmd('let g:gruvbox_material_background = "hard"')
-            vim.cmd('let g:gruvbox_material_better_performance = 1')
-            vim.cmd('let g:gruvbox_material_transparent_background = 2')
-            vim.cmd('let g:gruvbox_material_current_word = "bold"')
-            vim.cmd('let g:gruvbox_material_enable_italic = 1')
-            vim.cmd('colorscheme gruvbox-material')
-        end,
+            require("gruvbox").setup({
+                terminal_colors = true, -- add neovim terminal colors
+                undercurl = true,
+                underline = true,
+                bold = true,
+                italic = {
+                    strings = true,
+                    emphasis = true,
+                    comments = true,
+                    operators = false,
+                    folds = true,
+                },
+                strikethrough = true,
+                invert_selection = false,
+                invert_signs = false,
+                invert_tabline = false,
+                invert_intend_guides = false,
+                inverse = true, -- invert background for search, diffs,
+                contrast = "hard", -- can be "hard", "soft" or empty string
+                palette_overrides = {},
+                overrides = {},
+                dim_inactive = false,
+                transparent_mode = true,
+            })
+            vim.cmd("colorscheme gruvbox")
+        end
     },
 
     -----------------------
