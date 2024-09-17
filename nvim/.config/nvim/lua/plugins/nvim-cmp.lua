91cf2b396b80ce88160c31f3b475d977c6832380
Hristo Goumnerov <hristo.g@pharvision.ai>
2024-09-17 03:23:11 -0500

    Nvim - first attempt at snippets

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -19,7 +19,7 @@ return {
 
         local luasnip = require('luasnip')
 
-        require('luasnip.loaders.from_vscode').lazy_load()
+        require('luasnip.loaders.from_snipmate').lazy_load({paths = "~/.config/nvim/snippets"})
 
         cmp.setup({
             completion = {
