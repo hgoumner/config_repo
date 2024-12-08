0d6997e06ea79ad7652c44165c8c94f81cb0523a
Hristo Goumnerov <hristo.g@pharvision.ai>
2024-12-08 15:36:23 +0200

    Nvim - add none-ls and mypy

--- nvim/.config/nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -93,30 +93,6 @@ return {
                     args = {},
                 }
             },
-            commands = {
-                RuffAutofix = {
-                    function()
-                        vim.lsp.buf.execute_command {
-                            command = 'ruff.applyAutofix',
-                            arguments = {
-                                { uri = vim.uri_from_bufnr(0) },
-                            },
-                        }
-                    end,
-                    description = 'Ruff: Fix all auto-fixable problems',
-                },
-                RuffOrganizeImports = {
-                    function()
-                        vim.lsp.buf.execute_command {
-                            command = 'ruff.applyOrganizeImports',
-                            arguments = {
-                                { uri = vim.uri_from_bufnr(0) },
-                            },
-                        }
-                    end,
-                    description = 'Ruff: Format imports',
-                }
-            }
         }
 
         lspconfig["pyright"].setup({
