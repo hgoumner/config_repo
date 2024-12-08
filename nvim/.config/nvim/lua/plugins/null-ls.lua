f0857d968269d669b3c1836eb99d972ccd7208a1
Hristo Goumnerov <hristo.g@pharvision.ai>
2024-12-08 15:36:23 +0200

    Nvim - add none-ls and mypy

---
+++ nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -0,0 +1,13 @@
+return {
+    "nvimtools/none-ls.nvim",
+    ft = { "python" },
+    config = function()
+        local null_ls = require("null-ls")
+
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.diagnostics.mypy,
+            },
+        })
+    end
+}
