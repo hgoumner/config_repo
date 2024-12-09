ea5b89116dcf498b06395391796b0b91641e71cb
Hristo Goumnerov <hristo.g@pharvision.ai>
2024-12-09 18:12:34 +0200

    Nvim - add mypy python path for PV

--- nvim/.config/nvim/lua/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -6,7 +6,12 @@ return {
 
         null_ls.setup({
             sources = {
-                null_ls.builtins.diagnostics.mypy,
+                -- null_ls.builtins.diagnostics.mypy,
+                null_ls.builtins.diagnostics.mypy.with({
+                    extra_args = function()
+                        return { "--python-executable", "/home/hristo/.pyenv/shims/python3" }
+                    end,
+                })
             },
         })
     end
