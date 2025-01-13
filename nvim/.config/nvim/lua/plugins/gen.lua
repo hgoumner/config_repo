bfbbc30694a622e456616eef337bf620a6e54501
Hristo Goumnerov <hristo.g@pharvision.ai>
2025-01-13 23:16:23 +0200

    Nvim - set ollama options

--- nvim/.config/nvim/lua/plugins/gen.lua
+++ nvim/.config/nvim/lua/plugins/gen.lua
@@ -2,7 +2,7 @@ return {
 	"David-Kunz/gen.nvim",
 	lazy = false,
 	opts = {
-		model = "llama3.2", -- The default model to use.
+		model = "codellama:13b", -- The default model to use.
 		quit_map = "q", -- set keymap to close the response window
 		retry_map = "<c-r>", -- set keymap to re-send the current prompt
 		accept_map = "<c-cr>", -- set keymap to replace the previous selection with the last result
@@ -10,8 +10,8 @@ return {
 		port = "11434", -- The port on which the Ollama service is listening.
 		display_mode = "float", -- The display mode. Can be "float" or "split" or "horizontal-split".
 		show_prompt = false, -- Shows the prompt submitted to Ollama.
-		show_model = false, -- Displays which model you are using at the beginning of your chat session.
-		no_auto_close = false, -- Never closes the window automatically.
+		show_model = true, -- Displays which model you are using at the beginning of your chat session.
+		no_auto_close = true, -- Never closes the window automatically.
 		file = false, -- Write the payload to a temporary file to keep the command short.
 		hidden = false, -- Hide the generation window (if true, will implicitly set `prompt.replace = true`), requires Neovim >= 0.10
 		init = function(options)
