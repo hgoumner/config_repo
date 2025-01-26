ddeb1156171858d5cc78cb68f4684f7972cfe0dd
Hristo Goumnerov <hristo.g@pharvision.ai>
2025-01-26 17:11:02 +0200

    Fzf - small config update

--- fzf/.fzf.zsh
+++ fzf/.fzf.zsh
@@ -12,8 +12,6 @@ fi
 # ------------
 source "/home/$(whoami)/.fzf/shell/key-bindings.zsh"
 
-# export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude BraveSoftware --exclude .git'
-
 export FZF_CTRL_T_COMMAND='fd --type f --hidden --follow --exclude BraveSoftware --exclude .git'
 export FZF_CTRL_T_OPTS='--preview "bat --style=numbers --color=always --line-range :500 {}"'
 
@@ -24,3 +22,5 @@ export FZF_DEFAULT_OPTS='
     --color fg:-1,bg:-1,hl:230,fg+:3,bg+:233,hl+:229
     --color info:150,prompt:110,spinner:150,pointer:167,marker:174
 '
+
+source <(fzf --zsh)
