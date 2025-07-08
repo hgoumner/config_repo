7e1f2223fd7050f5074aea810ba8840dd76e966a
Hristo Goumnerov <hgoumnerov@tuta.io>
2025-07-08 13:41:15 +0300

    Nvim - update mcphub

--- nvim/.config/nvim/lua/plugins/mcphub.lua
+++ nvim/.config/nvim/lua/plugins/mcphub.lua
@@ -1,14 +1,12 @@
 return {
 	"ravitemer/mcphub.nvim",
 	dependencies = {
-		"nvim-lua/plenary.nvim", -- Required for Job and HTTP requests
+		"nvim-lua/plenary.nvim",
 	},
-	-- uncomment the following line to load hub lazily
-	--cmd = "MCPHub",  -- lazy load
-	build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
-	-- uncomment this if you don't want mcp-hub to be available globally or can't use -g
-	-- build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
+	build = "bundled_build.lua", -- Bundles `mcp-hub` binary along with the neovim plugin
 	config = function()
-		require("mcphub").setup()
+		require("mcphub").setup({
+			use_bundled_binary = true, -- Use local `mcp-hub` binary
+		})
 	end,
 }
