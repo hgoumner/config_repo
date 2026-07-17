10de2f6ee4a45b36cc08bf8a936b06510de8c7df
JF82788 <hristo.goumnerov@kbc.com>
2026-07-17 13:35:08 +0000

    feat(lazyvim): add centering for search

--- lazynvim/.config/nvim/lua/config/keymaps.lua
+++ lazynvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+map("n", "n", "nzz", { desc = "Center next match" })
+map("n", "N", "Nzz", { desc = "Center previous match" })
+map("n", "*", "*zz", { desc = "Center next match of current word" })
+map("n", "#", "#zz", { desc = "Center previous match of current word" })
