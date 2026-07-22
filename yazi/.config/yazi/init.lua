92dc196da7bcb830febca7695fa1f85989483a72
JF82788 <hristo.goumnerov@kbc.com>
2026-07-22 18:22:28 +0000

    feat(yazi): remove duckdb and use rich-preview for csv and json files

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,27 +1,6 @@
 -- You can configure your bookmarks by lua language
 local bookmarks = {}
 
-local path_sep = package.config:sub(1, 1)
-local home_path = ya.target_family() == "windows" and os.getenv("USERPROFILE") or os.getenv("HOME")
-if ya.target_family() == "windows" then
-	table.insert(bookmarks, {
-		tag = "Scoop Local",
-
-		path = (os.getenv("SCOOP") or home_path .. "\\scoop") .. "\\",
-		key = "p",
-	})
-	table.insert(bookmarks, {
-		tag = "Scoop Global",
-		path = (os.getenv("SCOOP_GLOBAL") or "C:\\ProgramData\\scoop") .. "\\",
-		key = "P",
-	})
-end
-table.insert(bookmarks, {
-	tag = "Desktop",
-	path = home_path .. path_sep .. "Desktop" .. path_sep,
-	key = "d",
-})
-
 require("yamb"):setup({
 	-- Optional, the path ending with path seperator represents folder.
 	bookmarks = bookmarks,
@@ -55,8 +34,5 @@ require("copy-file-contents"):setup({
 	append_char = "\n",
 	notification = true,
 })
-
--- DuckDB plugin configuration
-require("duckdb"):setup()
 
 require("simple-status"):setup()
