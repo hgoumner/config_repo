51e84c2b3230b2aad3a1ee713d4ec72688e415bd
JF82788 <hristo.goumnerov@kbc.com>
2026-08-05 11:19:28 +0000

    fix(fzf): remove unnecessary configuration

--- fzf/.fzf.zsh
+++ fzf/.fzf.zsh
@@ -1,17 +1,5 @@
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == */home/$(whoami)/.fzf/bin* ]]; then
-    export PATH="${PATH:+${PATH}:}/home/$(whoami)/.fzf/bin"
-fi
-
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/home/$(whoami)/.fzf/shell/completion.zsh" 2> /dev/null
-
 # Key bindings
 # ------------
-source "/home/$(whoami)/.fzf/shell/key-bindings.zsh"
-
 export FZF_CTRL_T_COMMAND='fd --type f --hidden --no-ignore-vcs --follow --exclude BraveSoftware --exclude .git --exclude .venv'
 export FZF_CTRL_T_OPTS='--preview "bat --style=numbers --color=always --line-range :500 {}"'
 
