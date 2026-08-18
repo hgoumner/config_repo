ab16c36bf710be4da9b467ae6fe2fcf9731a5c1c
JF82788 <hristo.goumnerov@kbc.com>
2026-08-18 12:20:04 +0000

    feat(yazi): add whoosh bookmark plugin and remove yamb

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,18 +1,3 @@
--- You can configure your bookmarks by lua language
-local bookmarks = {}
-
-require("yamb"):setup({
-	-- Optional, the path ending with path seperator represents folder.
-	bookmarks = bookmarks,
-	-- Optional, the cli of fzf.
-	cli = "fzf",
-	-- Optional, a string used for randomly generating keys, where the preceding characters have higher priority.
-	keys = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-	-- Optional, the path of bookmarks
-	path = (ya.target_family() == "windows" and os.getenv("APPDATA") .. "\\yazi\\config\\bookmark")
-		or (os.getenv("HOME") .. "/.config/yazi/bookmark"),
-})
-
 function Linemode:size_and_mtime()
 	local year = os.date("%Y")
 	local time = math.floor(self._file.cha.mtime or 0)
@@ -36,3 +21,21 @@ require("copy-file-contents"):setup({
 })
 
 require("simple-status"):setup()
+
+require("starship"):setup({
+	-- Hide flags (such as filter, find and search). This can be beneficial for starship themes
+	-- which are intended to go across the entire width of the terminal.
+	hide_flags = false,
+	-- Whether to place flags after the starship prompt. False means the flags will be placed before the prompt.
+	flags_after_prompt = true,
+	-- Custom starship configuration file to use
+	config_file = "~/config_repo/starship/starship_yazi.toml", -- Default: nil
+	-- Whether to enable support for starship's right prompt (i.e. `starship prompt --right`).
+	show_right_prompt = false,
+	-- Whether to hide the count widget, in case you want only your right prompt to show up. Only has
+	-- an effect when `show_right_prompt = true`
+	hide_count = false,
+	-- Separator to place between the right prompt and the count widget. Use `count_separator = ""`
+	-- to have no space between the widgets.
+	count_separator = " ",
+})
