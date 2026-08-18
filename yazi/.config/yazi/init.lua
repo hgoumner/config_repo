4dc3af007f7a7838c0997c8676d819b600d02cfb
JF82788 <hristo.goumnerov@kbc.com>
2026-08-18 12:19:28 +0000

    feat(yazi): add starship header line

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -27,7 +27,10 @@ function Linemode:size_and_mtime()
 	return ui.Line(string.format(" %s %s ", size and ya.readable_size(size) or "-", time))
 end
 
-require("git"):setup()
+require("git"):setup({
+	-- Order of status signs showing in the linemode
+	order = 1500,
+})
 
 require("copy-file-contents"):setup({
 	clipboard_cmd = "linux",
@@ -36,3 +39,21 @@ require("copy-file-contents"):setup({
 })
 
 require("simple-status"):setup()
+
+require("starship"):setup({
+	-- Hide flags (such as filter, find and search). This can be beneficial for starship themes
+	-- which are intended to go across the entire width of the terminal.
+	hide_flags = false,
+	-- Whether to place flags after the starship prompt. False means the flags will be placed before the prompt.
+	flags_after_prompt = true,
+	-- Custom starship configuration file to use
+	config_file = "~/config_repo/starship/starship_yazi.toml", -- Default: nil
+	-- Whether to enable support for starship's right prompt (i.e. `starship prompt --right`).
+	show_right_prompt = false,
+	-- Whether to hide the count widget, in case you want only your right prompt to show up. Only has
+	-- an effect when `show_right_prompt = true`
+	hide_count = false,
+	-- Separator to place between the right prompt and the count widget. Use `count_separator = ""`
+	-- to have no space between the widgets.
+	count_separator = " ",
+})
