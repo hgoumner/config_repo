09bcaf68ad4171b7227e5b71148cc956a1a191de
JF82788 <hristo.goumnerov@kbc.com>
2026-08-21 15:29:50 +0000

    feat(yazi): remove git and rich-preview

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -12,11 +12,6 @@ function Linemode:size_and_mtime()
 	return ui.Line(string.format(" %s %s ", size and ya.readable_size(size) or "-", time))
 end
 
-require("git"):setup({
-	-- Order of status signs showing in the linemode
-	order = 1500,
-})
-
 require("copy-file-contents"):setup({
 	clipboard_cmd = "linux",
 	append_char = "\n",
