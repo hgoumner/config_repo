b36c1685c67677c35af5187f8fccc3df837a4ecf
JF82788 <hristo.goumnerov@kbc.com>
2026-08-26 17:35:29 +0000

    feat(lazyvim): disable diagnostics by default

--- lazynvim/.config/nvim/init.lua
+++ lazynvim/.config/nvim/init.lua
@@ -1,6 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+vim.diagnostic.enable(false)
+
 vim.api.nvim_create_user_command("AirflowView", function()
   require("airflow_log").open()
 end, {
