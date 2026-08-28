a844dd4f4ccb6a45002074d6c4c266b84241a600
JF82788 <hristo.goumnerov@kbc.com>
2026-08-28 15:39:57 +0000

    feat(lazyvim): add bg line number coloring

--- lazynvim/.config/nvim/lua/plugins/theme.lua
+++ lazynvim/.config/nvim/lua/plugins/theme.lua
@@ -4,10 +4,15 @@ return {
     lazy = true,
     opts = {
       style = "night",
+      on_highlights = function (hl, c)
+        hl.LineNrAbove = { fg = "#ffffff" }
+        hl.CursorLineNr = { fg = "#297801" }
+        hl.LineNrBelow = { fg = "#ff0000" }
+      end
     },
   },
 
-  -- Configure LazyVim to load gruvbox
+  -- Configure LazyVim to load tokyonight
   {
     "LazyVim/LazyVim",
     opts = {
